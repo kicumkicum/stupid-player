c5ce85b11cfcc12c3192f64450663727dc6b4151
kicumkicum <kicumkicum@gmail.com>
2016-03-13 01:03:36 +0600

    Add test event play

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var url = 'http://picosong.com/cdn/7b64bb8716f6f35189b5fe1fba4efd12.mp3';
 
 
 describe('Creating', function() {
@@ -15,7 +16,7 @@ describe('Action', function() {
 	var stupidPlayer = new StupidPlayer;
 
 	describe('playing', function() {
-		it('play', function(done) {
+		it('async', function(done) {
 			stupidPlayer.play('http://storage.mp3.cc/download/22183477/aGxhQVE2VTlqWGFGTm1mOVNUcS9ZcWsrMXh5K2d1T0lPZ1hNOUlEMUZxOTVhVTVKcHIzbHRZUGgrWDlhU0RrYW5BVzhSdXJPcEQvMDhSWnRRWGVaVVJYWDB1V0g5TEcxclhxYlhyUExuRld0L2h3ditLNzFDQVkwSWVUOWNubVQ/Oksana_Pochepa_Akula-Melodrama_(mp3.cc).mp3')
 				.then(function () {
 					setTimeout(function () {
@@ -23,6 +24,16 @@ describe('Action', function() {
 						done();
 					}, 10000);
 				});
+		});
+
+		it('event', function(done) {
+			this.timeout(10 * 1000);
+			stupidPlayer.on(stupidPlayer.EVENT_START, function() {
+				assert.equal(StupidPlayer.State.PLAY, stupidPlayer._state, 'state');
+				done();
+			});
+
+			stupidPlayer.play(url);
 		});
 	});
 
