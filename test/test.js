d5ecc6e61073e87dc4858f1d80c78fcd3cd5fa28
kicumkicum <kicumkicum@gmail.com>
2016-03-14 22:53:38 +0600

    Update

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
+var StupidPlayer = require('../index');
 var assert = require('assert');
 var url = 'http://www.stephaniequinn.com/Music/Allegro%20from%20Duet%20in%20C%20Major.mp3';
 
 
 describe('Creating', function() {
 	it('create', function() {
-		var StupidPlayer = require('../index');
 		var stupidPlayer = new StupidPlayer;
 		assert.equal(true, stupidPlayer instanceof StupidPlayer);
 	});
@@ -12,7 +12,6 @@ describe('Creating', function() {
 
 
 describe('Action', function() {
-	var StupidPlayer = require('../index');
 	var stupidPlayer = new StupidPlayer;
 
 	describe('playing', function() {
@@ -37,18 +36,21 @@ describe('Action', function() {
 
 	describe('stopping', function() {
 		it('async play-stop', function(done) {
-			stupidPlayer.play('http://storage.mp3.cc/download/22183477/aGxhQVE2VTlqWGFGTm1mOVNUcS9ZcWsrMXh5K2d1T0lPZ1hNOUlEMUZxOTVhVTVKcHIzbHRZUGgrWDlhU0RrYW5BVzhSdXJPcEQvMDhSWnRRWGVaVVJYWDB1V0g5TEcxclhxYlhyUExuRld0L2h3ditLNzFDQVkwSWVUOWNubVQ/Oksana_Pochepa_Akula-Melodrama_(mp3.cc).mp3')
+			stupidPlayer.play(url)
 				.then(function() {
+					assert.equal(StupidPlayer.State.PLAY, stupidPlayer._state, 'state');
 					stupidPlayer.stop();
 					assert.equal(StupidPlayer.State.STOP, stupidPlayer._state, 'state');
 					done();
 				});
 		});
 
-		it('sync play-stop', function(done) {
-			stupidPlayer.play('http://storage.mp3.cc/download/22183477/aGxhQVE2VTlqWGFGTm1mOVNUcS9ZcWsrMXh5K2d1T0lPZ1hNOUlEMUZxOTVhVTVKcHIzbHRZUGgrWDlhU0RrYW5BVzhSdXJPcEQvMDhSWnRRWGVaVVJYWDB1V0g5TEcxclhxYlhyUExuRld0L2h3ditLNzFDQVkwSWVUOWNubVQ/Oksana_Pochepa_Akula-Melodrama_(mp3.cc).mp3');
+		it('sync play-stop', function() {
+			this.timeout(10 * 1000);
+			stupidPlayer.play(url);
+			assert.equal(StupidPlayer.State.PLAY, stupidPlayer._state, 'state');
 			stupidPlayer.stop();
-			setTimeout(done, 10000);
+			assert.equal(StupidPlayer.State.STOP, stupidPlayer._state, 'state');
 		});
 	});
 });
