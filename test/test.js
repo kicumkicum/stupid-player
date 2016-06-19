ab507e065143a6f8fd700a657324b1b25b7cd4ec
Oleg Akinin <kicumkicum@gmail.com>
2016-06-19 22:48:14 +0600

    #19 Add tests (#31)
    * Add test.js with simple tests
    
    * Add command for run test in package.json
    
    * Remove only for it
    
    * Add .travis.yml
    
    * Update test command for travis
    
    * Update test command for travis
    
    * Add test event play
    
    * Add typedef for chai and mocha
    
    * Add test
    
    * Add test
    
    * Update
    
    * Fix firing error, if url is not correct. Refactor *StupidPlayer.prototype._checkConnect*
    
    * Add testing not correct url
    
    * Add test for double play after error
    
    * Fix crash after double play after error
    
    * Merge branch 'develop' into feature/19-add-tests
    
    # Conflicts:
    #	lib/stupid-player.js
    
    * Fix test with event play
    
    * Skip test
    
    * Fix getting state
    
    * Add node v6.2 for travis
    
    * Version 0.2.0

---
+++ test/test.js
@@ -0,0 +1,109 @@
+var StupidPlayer = require('../index');
+var assert = require('assert');
+var url = 'http://www.stephaniequinn.com/Music/Allegro%20from%20Duet%20in%20C%20Major.mp3';
+
+
+describe('Creating', function() {
+	it('create', function() {
+		var stupidPlayer = new StupidPlayer;
+		assert.equal(true, stupidPlayer instanceof StupidPlayer);
+	});
+});
+
+
+describe('Action', function() {
+	var stupidPlayer = new StupidPlayer;
+
+	describe('playing', function() {
+		it('async', function(done) {
+			stupidPlayer.play(url)
+				.then(function() {
+					assert.equal(StupidPlayer.State.PLAY, stupidPlayer._state, 'state');
+					done();
+				});
+		});
+
+		it('event', function(done) {
+			this.timeout(10 * 1000);
+			stupidPlayer.on(stupidPlayer.EVENT_PLAY, function() {
+				assert.equal(StupidPlayer.State.PLAY, stupidPlayer._state, 'state');
+				done();
+			});
+
+			stupidPlayer.play(url);
+		});
+	});
+
+	describe('stopping', function() {
+		it('async play-stop', function(done) {
+			stupidPlayer.play(url)
+				.then(function() {
+					assert.equal(StupidPlayer.State.PLAY, stupidPlayer.state, 'state');
+					stupidPlayer.stop();
+					assert.equal(StupidPlayer.State.STOP, stupidPlayer.state, 'state');
+					done();
+				});
+		});
+
+		it.only('sync play-stop', function() {
+			this.timeout(10 * 1000);
+			stupidPlayer.play(url);
+			assert.equal(StupidPlayer.State.PLAY, stupidPlayer.state, 'state');
+			stupidPlayer.stop();
+			assert.equal(StupidPlayer.State.STOP, stupidPlayer.state, 'state');
+		});
+	});
+
+	describe('Multiple playing', function() {
+		describe('Url is not correct', function() {
+			it.skip('Sync playing', function(done) {
+				var stupidPlayer = new StupidPlayer;
+				var url = 'http://ya.ru/';
+				var eventsCount = {
+					play: 0,
+					stop: 0,
+					error: 0
+				};
+				this.timeout(20 * 1000);
+
+				stupidPlayer.on(stupidPlayer.EVENT_ERROR, function() {
+					eventsCount.error++;
+					stupidPlayer.play(url);
+				});
+				stupidPlayer.on(stupidPlayer.EVENT_PLAY, function() {
+					eventsCount.play++;
+				});
+				stupidPlayer.on(stupidPlayer.EVENT_STOP, function() {
+					eventsCount.stop++;
+				});
+
+				stupidPlayer.play(url);
+				setTimeout(function() {
+					assert.equal(eventsCount.error, eventsCount.play - 1, 'Error fired after not all play-events');
+					assert.equal(eventsCount.stop, 1, 'Stop was fired');
+					done();
+				}, 19 * 1000);
+			});
+
+			it('Sync double playing', function(done) {
+				var stupidPlayer = new StupidPlayer;
+				var url = 'http://ya.ru/';
+				this.timeout(20 * 1000);
+
+				stupidPlayer.on(stupidPlayer.EVENT_ERROR, function() {
+					stupidPlayer.play(url);
+					stupidPlayer.play(url);
+				});
+
+				stupidPlayer.play(url);
+				setTimeout(function() {
+					done();
+				}, 19 * 1000);
+			});
+
+			it('Async playing', function() {
+
+			});
+		});
+	});
+});
