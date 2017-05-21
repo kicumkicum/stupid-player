9936bae1410291e2dd3a1587816997344b010389
reeFridge <reefridgerator@gmail.com>
2017-05-21 15:27:49 +0700

    Rework magnet rout

--- src/routes/torrent.ts
+++ src/routes/torrent.ts
@@ -1,26 +1,36 @@
 import * as torrentStream from 'torrent-stream';
 import {Readable} from 'stream';
+import {decode as decodeMagnetURI} from 'magnet-uri';
 
 
 export default {
-	regexp: /^magnet*:/,
+	test(str: string): boolean {
+		return !!Object.keys(decodeMagnetURI(str)).length;
+	},
 
 	read(uri: string): Promise<FileStream> {
 		return new Promise((resolve, reject) => {
-			// TODO: Символ решетки не уникальный разделитель
-			const [magnetUri, path] = uri.split('#');
-			const engine: TorrentStream.TorrentEngine = torrentStream(magnetUri);
+			const decodedUri = decodeMagnetURI(uri);
+			const encodedFilePath = decodedUri['x.filepath'];
 
-			engine.on('ready', () => {
-				const files = engine.files;
-				const file = files.filter((file) => file.path === path)[0];
+			if (encodedFilePath) {
+				const filePath = decodeURI(encodedFilePath);
+				const engine: TorrentStream.TorrentEngine = torrentStream(uri);
 
-				if (file) {
-					resolve(file.createReadStream());
-				} else {
-					reject('File is not found');
-				}
-			});
+				engine.on('ready', () => {
+					const files = engine.files;
+					const file = files
+						.filter((file) => file.path === filePath)[0];
+
+					if (file) {
+						resolve(file.createReadStream());
+					} else {
+						reject('File is not found');
+					}
+				});
+			} else {
+				// TODO: Play all mp3 files
+			}
 		});
 	}
 }
