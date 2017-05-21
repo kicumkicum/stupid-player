6475b3c5912d88c40c668def4fae15f71e4168a5
kicumkicum <kicumkicum@gmail.com>
2017-05-21 13:53:54 +0700

    Replace prop regexp to test for IRoute interface

--- src/routes/http.ts
+++ src/routes/http.ts
@@ -1,11 +1,10 @@
 import * as http from 'http';
 import * as https from 'https';
 
-
 export default {
-	regexp: /^https*:\/\//,
+	test: (uri: string): boolean => /^https*:\/\//.test(uri),
 
-	read(url: string): Promise<http.IncomingMessage> {
+	read: (url: string): Promise<http.IncomingMessage> => {
 		return new Promise((resolve, reject) => {
 			const get = url.indexOf('https') === 0 ? https.get : http.get;
 
